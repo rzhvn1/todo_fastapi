4aae5d1ba8ed92da4b1a0b7ea9ae7cfde411ac0e
Erzhan <erzhanmuratov99@gmail.com>
2025-03-03 16:17:57 +0600

    fix: assign owner by client

--- app/api/v1/routes/tasks.py
+++ app/api/v1/routes/tasks.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from schemas.task import TaskResponse, TaskCreate
 from dependencies.db import SessionDep
-from dependencies.user import CurrentUser
+from dependencies.user import CurrentUser, TokenDep
 from models.task import Task
+from models.user import User
 
 
 router = APIRouter()
@@ -13,11 +14,14 @@ async def create_task(
     session: SessionDep,
     current_user: CurrentUser,
 ) -> TaskResponse:
+    owner = session.get(User, task_in.owner_id)
+    if not owner:
+        raise HTTPException(status_code=404, detail="User not found")
     task = Task(
         title=task_in.title,
         description=task_in.description,
         is_completed=task_in.is_completed,
-        owner_id=current_user.id,
+        owner_id=owner.id
     )
     session.add(task)
     session.commit()
